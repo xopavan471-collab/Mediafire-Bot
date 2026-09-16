e4808d50bfbe705ebf8b4dfae00a017c0d1594c4
xopavan471-collab <xopavan471@gmail.com>
2026-09-16 14:53:54 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from pyrogram import Client, filters
+import os
+
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+app = Client("mediafire-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply("Bhai Bot ON hai! File bhejo, mai MediaFire pe daal dunga 🚀")
+
+app.run()
